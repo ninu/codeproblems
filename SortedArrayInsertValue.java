7d76b552b80eea79f28e1f47599edb43e95fc8fc
Nirdhar Khazanie <ninu@ninu.net>
2014-09-29 19:14:28 -0700

    sortedarrayinsert

---
+++ SortedArrayInsertValue.java
@@ -0,0 +1,21 @@
+/*
+given a sorted array of positive integers with an empty spot (zero) at the end, insert an element in sorted order
+*/
+
+public class SortedArrayInsertValue{
+	public static void main(String[]args){
+		int num[] = {1,7,8,19,25,0};
+		int val = 12;
+		int position = num.length-1;
+		while(val < num[position-1]){
+			num[position] = num[position - 1];
+			position--;
+		}
+		//System.out.println(counter);
+		num[position] = val;
+		
+		for(int i=0; i<num.length; i++){
+			System.out.println(num[i]);
+		}
+	}
+}
