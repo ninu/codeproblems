932b210780cf22cfeb4d796e34da042ee068e8f5
Nirdhar Khazanie <ninu@ninu.net>
2014-10-19 00:06:18 -0400

    Update and rename SortedArrayInsertValue.java to SortedArrayInsertValueAtEnd.java

--- SortedArrayInsertValueAtEnd.java
+++ SortedArrayInsertValueAtEnd.java
@@ -2,7 +2,7 @@
 given a sorted array of positive integers with an empty spot (zero) at the end, insert an element in sorted order
 */
 
-public class SortedArrayInsertValue{
+public class SortedArrayInsertValueAtEnd{
 	public static void main(String[]args){
 		int num[] = {1,7,8,19,25,0};
 		int val = 12;
